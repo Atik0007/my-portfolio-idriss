966d137b5f0585e6297d7458a204bcccbea76559
Idriss Atik <idriss.atik@LSTKCO108260.local>
2024-01-02 13:35:02 +0100

    add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
+import Hero from './components/main/Hero';
 
 export default function Home() {
   return (
-   <main>
-    test
-   </main>
-  )
+    <main className="h-ful w-full">
+      <div className="flex flex-col h-[850px] gap-20">
+        <Hero />
+      </div>
+    </main>
+  );
 }
