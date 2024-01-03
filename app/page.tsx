59ee4b9787a12a209006e51274a181a1cfd843d3
Idriss Atik <idriss.atik@LSTKCO108260.local>
2024-01-03 11:12:13 +0100

    add skills

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import Hero from './components/main/Hero';
-
+import Skills from './components/main/Skills';
 export default function Home() {
   return (
     <main className="h-ful w-full">
       <div className="flex flex-col h-[850px] gap-20">
-        
         <Hero />
+        <Skills />
       </div>
     </main>
   );
